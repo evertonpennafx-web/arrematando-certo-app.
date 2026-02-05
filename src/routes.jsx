5a8293728ff26c5e9a1839de613473f4c4d86676
evertonpennafx-web <evertonpennafx@gmail.com>
2026-02-05 03:19:16 -0300

    Update routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,31 +1,31 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import HomePage from './pages/HomePage';
-import FreeTastingPage from './pages/FreeTastingPage';
-import ConsultationPage from './pages/ConsultationPage';
-import ConsultationLandingPage from './pages/ConsultationLandingPage';
-import EnviarEditalPage from './pages/EnviarEditalPage';
-import FormSuccessPage from './pages/FormSuccessPage';
-import SubmissionPage from './pages/SubmissionPage';
-import RelatorioPage from './pages/RelatorioPage';
-import SuccessPage from './pages/SuccessPage';
-import PaymentSuccess from './pages/PaymentSuccess';
-import PaymentCancel from './pages/PaymentCancel';
+import HomePage from "./pages/HomePage";
+import EnviarEditalPage from "./pages/EnviarEditalPage";
+import SubmissionPage from "./pages/SubmissionPage";
+import RelatorioPage from "./pages/RelatorioPage";
+import SuccessPage from "./pages/SuccessPage";
+import FormSuccessPage from "./pages/FormSuccessPage";
+import PaymentSuccess from "./pages/PaymentSuccess";
+import PaymentCancel from "./pages/PaymentCancel";
+import ConsultationPage from "./pages/ConsultationPage";
+import ConsultationLandingPage from "./pages/ConsultationLandingPage";
+import FreeTastingPage from "./pages/FreeTastingPage";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/degustacao-gratuita" element={<FreeTastingPage />} />
-      <Route path="/consultoria" element={<ConsultationPage />} />
-      <Route path="/consultoria/landing" element={<ConsultationLandingPage />} />
       <Route path="/enviar-edital" element={<EnviarEditalPage />} />
-      <Route path="/form-sucesso" element={<FormSuccessPage />} />
       <Route path="/submission" element={<SubmissionPage />} />
       <Route path="/relatorio" element={<RelatorioPage />} />
       <Route path="/sucesso" element={<SuccessPage />} />
-      <Route path="/pagamento/sucesso" element={<PaymentSuccess />} />
-      <Route path="/pagamento/cancelado" element={<PaymentCancel />} />
+      <Route path="/form-sucesso" element={<FormSuccessPage />} />
+      <Route path="/pagamento-sucesso" element={<PaymentSuccess />} />
+      <Route path="/pagamento-cancelado" element={<PaymentCancel />} />
+      <Route path="/consultoria" element={<ConsultationPage />} />
+      <Route path="/consultoria-inicio" element={<ConsultationLandingPage />} />
+      <Route path="/free" element={<FreeTastingPage />} />
 
       {/* fallback */}
       <Route path="*" element={<HomePage />} />
