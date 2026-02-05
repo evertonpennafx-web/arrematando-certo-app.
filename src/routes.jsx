8345f2c9490aae429e4a90eff8f42989d85a5091
evertonpennafx-web <evertonpennafx@gmail.com>
2026-02-05 13:47:45 -0300

    Update routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,17 +1,15 @@
 import { Routes, Route } from "react-router-dom";
-
-function Home() {
-  return (
-    <div style={{ minHeight: "100vh", background: "#111", color: "#fff", padding: 24 }}>
-      HOME ARREMATANDO CERTO ✅
-    </div>
-  );
-}
+import Home from "./pages/Home";
+import Analisar from "./pages/Analisar";
+import Resultado from "./pages/Resultado";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/analisar" element={<Analisar />} />
+      <Route path="/resultado" element={<Resultado />} />
+      <Route path="*" element={<Home />} />
     </Routes>
   );
 }
