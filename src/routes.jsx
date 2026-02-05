02bde96519c41ea5d55667d2eea5682516e16904
evertonpennafx-web <evertonpennafx@gmail.com>
2026-02-05 15:29:23 -0300

    Update routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,20 +1,34 @@
 import { Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
+
 import Analisar from "./pages/Analisar";
 import Resultado from "./pages/Resultado";
+
+import FreeTastingPage from "./pages/FreeTastingPage";
+import ConsultationPage from "./pages/ConsultationPage";
+import EnviarEditalPage from "./pages/EnviarEditalPage";
+import SubmissionPage from "./pages/SubmissionPage";
 
 export default function AppRoutes() {
   return (
     <Routes>
-      {/* HOME PRINCIPAL (BONITA) */}
+      {/* HOME BONITA */}
       <Route path="/" element={<HomePage />} />
 
-      {/* MVP */}
+      {/* ROTAS DOS BOTÕES */}
+      <Route path="/degustacao-gratuita" element={<FreeTastingPage />} />
+      <Route path="/consultoria" element={<ConsultationPage />} />
+      <Route path="/enviar-edital" element={<EnviarEditalPage />} />
+
+      {/* PLANOS (usa query string ?plan=standard|express) */}
+      <Route path="/submission" element={<SubmissionPage />} />
+
+      {/* MVP IA */}
       <Route path="/analisar" element={<Analisar />} />
       <Route path="/resultado" element={<Resultado />} />
 
-      {/* fallback */}
+      {/* FALLBACK */}
       <Route path="*" element={<HomePage />} />
     </Routes>
   );
