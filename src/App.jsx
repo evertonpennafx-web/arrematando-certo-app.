31ed3aa7d5ea185ab3656ba3f3958d5c44471409
evertonpennafx-web <evertonpennafx@gmail.com>
2026-02-05 03:10:01 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 import { BrowserRouter } from 'react-router-dom';
+import AppRoutes from './routes';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <div
-        style={{
-          background: '#000',
-          color: '#00ff88',
-          minHeight: '100vh',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          fontSize: '24px',
-          fontFamily: 'sans-serif',
-        }}
-      >
-        ✅ APP CARREGOU COM SUCESSO
-      </div>
+      <AppRoutes />
     </BrowserRouter>
   );
 }
