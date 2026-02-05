34a48425a29ba517e2f552ec3a6776c8ed904f93
evertonpennafx-web <evertonpennafx@gmail.com>
2026-02-05 13:29:16 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter } from "react-router-dom";
-import Routes from "./routes";
+import AppRoutes from "./routes";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes />
+      <AppRoutes />
     </BrowserRouter>
   );
 }
