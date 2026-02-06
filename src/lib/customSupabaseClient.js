af88f0243eab382a1e291b6ab31bb3fbf7902755
evertonpennafx-web <evertonpennafx@gmail.com>
2026-02-06 13:36:41 -0300

    Update customSupabaseClient.js

--- src/lib/customSupabaseClient.js
+++ src/lib/customSupabaseClient.js
@@ -1,13 +1,5 @@
-import { createClient } from '@supabase/supabase-js';
+import { supabase } from "./supabase";
 
-const supabaseUrl = 'https://dybzykolilsuxewkulbq.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR5Ynp5a29saWxzdXhld2t1bGJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3Mzc3NDcsImV4cCI6MjA4NTMxMzc0N30.j3HWqGaJRODLS_ORlkY3cKy6DN76BW9USnR1Sh1wtDs';
-
-const customSupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
-
-export default customSupabaseClient;
-
-export { 
-    customSupabaseClient,
-    customSupabaseClient as supabase,
-};
+// ✅ Mantém compatibilidade com imports antigos
+export default supabase;
+export const customSupabaseClient = supabase;
