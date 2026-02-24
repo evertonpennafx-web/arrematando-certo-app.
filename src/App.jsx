0aefb639b5db08cc389d078b1f7f701f7133bb8a
evertonpennafx-web <evertonpennafx@gmail.com>
2026-02-24 10:58:36 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import { BrowserRouter } from "react-router-dom";
+import { useEffect } from "react";
 import AppRoutes from "./routes";
 
 export default function App() {
+
+  useEffect(() => {
+    // META PIXEL
+    const script = document.createElement("script");
+    script.innerHTML = `
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '1450461643272272');
+      fbq('track', 'PageView');
+    `;
+    document.head.appendChild(script);
+  }, []);
+
   return (
     <BrowserRouter>
       <AppRoutes />
